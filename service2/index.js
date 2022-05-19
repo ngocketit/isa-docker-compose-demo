1c8bbf5cbdffa866be3cc9342dbc789c8dcd3c9e
Phi Van Ngoc <ngocketit@gmail.com>
2022-05-19 23:36:56 +0300

    Fix wrong url

--- service2/index.js
+++ service2/index.js
@@ -4,11 +4,12 @@ const app = express()
 const port = 9002
 
 app.get('/message/:name', (req, res) => {
-  axios.get(`service1/greet/${req.params.name}`).then(resp => {
+  axios.get(`http://service1:9001/greet/${req.params.name}`).then(resp => {
     res.json({
       message: resp.data.message,
     })
-  }).catch(_ => {
+  }).catch(err => {
+    console.error(err)
     res.status(500).json({
       error: 'Something went wrong',
     })
