aec4dac90b2875f8247f045c044b89c2d2c3d260
Phi Van Ngoc <ngocketit@gmail.com>
2022-05-20 00:06:59 +0300

    Add basic-auth folder if not exists

--- gen-httppass.sh
+++ gen-httppass.sh
@@ -3,6 +3,8 @@
 read -p "Enter username: " uname
 read -s -p "Enter password: " passwd
 
+[[ ! -d ./basic-auth ]] && mkdir ./basic-auth
+
 for svc in service1 service2; do
   htpasswd -b -c ./basic-auth/$svc.local $uname $passwd
 done
